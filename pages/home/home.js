c6ac614a272160a771e5087f47563e5813c668f7
Norgannon <2300776402@qq.com>
2020-06-26 16:10:40 +0800

    Initial Commit

---
+++ pages/home/home.js
@@ -0,0 +1,112 @@
+Page({
+  data: {
+    cl:"#f6f6f6",
+    src:'',
+    button_text:"选择图片",
+    flag:1,
+    change_text:"品种识别",
+    flag2:1,
+    array: ['人', '鸟', '猫', '狗', '马', '羊', '牛', '象', '熊'],
+    index:0
+  },
+  bindViewTap: function () {
+    if(this.data.flag==1){
+      if (this.data.flag2 == 1) {
+        var u = 'http://175.24.53.216/up.php'
+      }
+      else {
+        var u = 'http://175.24.53.216/up2.php'
+      }
+      var that = this;
+      wx.chooseImage({
+        count: 1,
+        sizeType: ['original', 'compressed'],
+        success: function (res) {
+          that.setData({ flag: 0, button_text: "已上传"})
+          var tempFilePaths = res.tempFilePaths
+          let src = res.tempFilePaths[0]
+          that.setData({ src: src })
+          var up = wx.uploadFile({
+            url: u, //仅为示例，非真实的接口地址
+            filePath: tempFilePaths[0],
+            name: 'file',
+            formData: {
+              method: 'POST',   //请求方式
+              'item':that.data.index 
+            },
+            success: function (res) {
+              that.setData({ flag: 1, button_text: "选择图片" })
+              var data = res.data
+              console.log(data)
+              wx.showModal({
+                title: '提示',
+                content: data,
+              })
+            },
+            fail: function (error) {
+              that.setData({ flag: 1, button_text: "选择图片" })
+              console.log(error)
+              wx.showModal({
+                title: '提示',
+                content: error,
+              })
+            }
+          })
+        }
+      })
+    }
+    else{
+      wx.showModal({
+        title: '提示',
+        content: "图片已上传，请稍后",
+      })
+    }
+  },
+  preview_img: function () {
+    wx.previewImage({
+      current: this.data.src, // 当前显示图片的http链接
+      urls: this.data.src // 需要预览的图片http链接列表
+    })
+  },
+  change: function () {
+    if(this.data.flag2==1)
+    {
+      this.setData({ flag2: 0, change_text: "物体检测" ,cl:"#000000"})
+    }
+    else{
+      this.setData({ flag2: 1, change_text: "品种识别", cl: "#f6f6f6"})
+    }
+    
+  },
+  bindPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  button_down() {
+    wx.request({
+      url: 'http://175.24.53.216/yolo/test.php',
+      data: {
+        userName: 'shit',
+      },
+      method: 'GET',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        wx.showModal({
+          title: '提示',
+          content: res.data,
+        })
+      },
+      fail: function (res) {
+        wx.showModal({
+          title: '提示',
+          content: res.data,
+        })
+      }
+    })
+  },
+})
